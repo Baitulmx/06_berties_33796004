af2b4ca2274302e1b867894742e677b2996c9de0
SAslam <soubhaan324@gmail.com>
2025-11-21 00:35:47 +0000

    make changes for task 7 with user 'gold'

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,4 +2,13 @@
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+# Insert sample books
+INSERT INTO books (name, price)
+VALUES 
+('Brighton Rock', 20.25),
+('Brave New World', 25.00),
+('Animal Farm', 12.99);
+
+INSERT INTO users (username, first, last, email, hashedPassword)
+VALUES 
+('gold', 'Gold', 'Smiths', 'gold@example.com', '$2b$10$FO/0EvtT9jXqxDvkBklNPe4LckM3eshALA7hoAsenLN4wGs0as0I6');
